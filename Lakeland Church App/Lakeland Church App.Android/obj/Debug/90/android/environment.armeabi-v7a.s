df708cc8e3a2fa3a58561432bd0b0c34f7e6b065
Toby Samples <toby.samples@gmail.com>
2020-05-13 15:11:15 -0500

    Update Homepage label

--- Lakeland Church App/Lakeland Church App.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ Lakeland Church App/Lakeland Church App.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"2920d50a-eeef-431b-b230-14f319666204"
+	.asciz	"effa1022-1e9e-437c-8508-8d69f3898ea3"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
